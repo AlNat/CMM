e07feab03c9198f0441f24b2d73a47e660f10db7
AlNat <aleksnatarov@gmail.com>
2016-03-31 01:31:04 +0300

    Complier structure Language structure

---
+++ src/Main.java
@@ -0,0 +1,12 @@
+public class Main {
+
+    public static void main(String[] args) {
+
+        Lexer lex = new Lexer();
+        Parser par = new Parser();//lex);
+        //par.pars();
+
+
+
+    }
+}
---
+++ src/Parser.java
@@ -0,0 +1,13 @@
+/**
+ * Created by AlNat on 31.03.2016.
+ */
+
+//package Parser;
+
+public class Parser {
+
+    public Parser () {
+
+    }
+
+}
