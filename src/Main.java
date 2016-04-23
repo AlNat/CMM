624ac522c79ed817622b88f03aff1ed5d33829c6
AlNat <aleksnatarov@gmail.com>
2016-04-24 01:10:24 +0300

    Tokenizer complete Lexer complete main and commentary

--- src/Main.java
+++ src/Main.java
@@ -1,12 +1,24 @@
+import java.io.IOException;
+import java.util.Scanner;
+
 public class Main {
 
     public static void main(String[] args) {
 
+        Scanner scanner = new Scanner(System.in);
+        String filename;
+
+        System.out.println("Please, input filename");
+        //filename = scanner.nextLine();
+        filename = "D:\\2.cmm"; // Временно
         Lexer lex = new Lexer();
-        Parser par = new Parser();//lex);
-        //par.pars();
+        Parser par = new Parser();
 
-
+        try {
+           lex.Parse(filename);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
 
     }
 }
--- src/Tokenizer.java
+++ src/Tokenizer.java
@@ -1,13 +1,77 @@
+import java.io.IOException;
+
+import java.nio.charset.StandardCharsets;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+
+import java.util.ArrayList;
+import java.util.List;
+
 /**
- * Created by AlNat on 31.03.2016.
+ * Created by AlNat on 20.04.2016.
  */
 
 //package Lexer;
 
 public class Tokenizer {
 
+    private ArrayList<String> tokens; // Массив числовых представлений токенов
+    private List<String> file; // Наш файл с программа для распарсивания
+    private int currentToken; // Указатель на текущий токен
+
     public Tokenizer () {
 
+        tokens = new ArrayList<>();
+        currentToken = 0;
+    }
+
+    public String GetNextToken () {
+
+        currentToken++;
+        if (currentToken  > tokens.size() ) { // Если больше токенов нет
+            System.out.println("OUT OF BOUND! #" + currentToken);
+            return "ERROR";
+        }
+
+        return tokens.get(currentToken - 1);
+
+    }
+
+    public String GetPrevToken () {
+
+        return tokens.get(currentToken - 2);
+
+    }
+
+    public void SetTokenPosition (int index) {
+        currentToken = index;
+    }
+
+    private void addToken (String token) {
+        tokens.add(token);
+        currentToken++;
+    }
+
+    public void Parse (String filename) throws IOException {
+
+        file = Files.readAllLines(Paths.get(filename), StandardCharsets.UTF_8); // Прочитали весь файл
+        String[] tokensInFile; // Массив строковых токенов
+        for (String st: file) {
+            tokensInFile = st.split(" "); // Расрпарсили строки, по правилу через regex
+            // Теперь в tokensInFile лежут куча текстовых токенов для конкретной строки
+
+            for (String st2: tokensInFile) { // Для всех распарсенных строк
+                String t = st2.replaceAll("[\\n\\t\\r\\s ]+", "");// Заменили все пробелы на ничто
+                if (t.equals("")) { // Чтобы пстые строчки не добавлялись
+                    continue;
+                }
+                addToken( t ); // И добавили его
+                //System.out.print("Token #" + currentToken + " => ");
+                //System.out.println (t);
+            }
+        }
+
+        currentToken = 0;
     }
 
 
